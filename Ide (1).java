69beae0c7b22e3d7e428e25b705a2de111619791
AbhinavRathore <rathore2015@gmail.com>
2017-10-31 20:08:16 +0530

    Update Ide (1).java

--- Ide (1).java
+++ Ide (1).java
@@ -3,7 +3,7 @@ import java.lang.*;
 import java.io.*;
 import java.math.BigInteger;
  
-/* Name of the class has to be "Main" only if the class is public. */
+/* No need to use big integer....use long long int */
 class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
