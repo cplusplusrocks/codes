c8828ecd5243d0ec726cfe79daa2bec89d35bad1
cplusplusrocks <33257454+cplusplusrocks@users.noreply.github.com>
2017-10-31 20:02:44 +0530

    Update Ide (7).cpp
    SEGMENT TREE will decrease complexity

--- Ide (7).cpp
+++ Ide (7).cpp
@@ -1,5 +1,60 @@
 #include <bits/stdc++.h>
 using namespace std;
+void build(int ss , int se , int i)
+{
+	if(ss == se){
+		t[i] = a[ss];
+		return ; 
+	}
+	int mid = (ss+se)/2;
+	build(ss , mid , 2*i+1);
+	build(mid+1 , se , 2*i+2); 
+	t[i] = t[2*i+1] + t[2*i+2]; 
+}
+void get(int ss ,int se ,int qs , int qe , int i)
+{
+	if(lazy[i] > 0) propagate(ss , se , i);
+	if(qs > se || qe < ss) return 0;
+	if(qs  ss && qe > =se) return t[i];
+	int mid = (ss+se)/2;
+	return get(ss , mid ,  qs , qe , 2*i+1) + 
+		get(mid+1, se , qs , qe , 2*i+2); 
+}
+
+void propagate(int ss , int se , int i)
+{
+	lazy[i] = lazy[i] % 10 ;
+	tree[i].sum =0 ;  
+	for(int j =0  ; j < 10 ; j++){
+		cnt1[j] = tree[i].cnt[(j - lazy[i] + 10) % 10]; 
+	}
+	for(int j =0 ; j < 10 ; j++){
+		tree[i].cnt[j] = cnt1[j]; 
+		tree[i].sum+= cnt1[j] * j ; 
+	}
+	if(ss != se)
+	{
+		lazy[2*i+1]+= lazy[i];
+		lazy[2*i+2]+= lazy[i]; 
+	}
+	lazy[i] = 0 ; 
+}
+
+void update(int ss , int se  ,int qs, int qe , int i,int val)
+{
+	if(lazy[i] > 0 ) propagate(ss , se , i ); 
+	if(qs > se || qe < ss) return ; 
+	if(qs <= ss && qe>= se) 
+	{
+		lazy[i]+= val ;
+		propagate(ss , se , i);
+		return ;  
+	}
+	int mid = (ss + se)/2;
+	update(ss , mid ,qs , qe , 2*i+1,val);
+	update(mid+1 , se , qs, qe , 2*i+2,val);
+	t[i] = t[2*i+1]+t[2*i+2]; 
+}
 
 int main() {
 	// your code goes here
@@ -114,9 +169,8 @@ int main() {
 	    {
 	        if(final[i][j]<min)
 	        min=final[i][j];
-	       // printf("%d,",final[i][j]);
+	       
 	    }
-	   // printf("\n");
 	    }
 	 printf("%d",min);
 
